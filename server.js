8fc74c85d59de9c1ab72ac5ecb09c68f1c3d9568
LEE-YO-HAN <yhl0078@gmail.com>
2023-01-06 04:12:25 +0900

    init

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
+const fs = require("fs");
+const os = require("os");
+
+fs.copyFile("db.json", os.tmpdir() + "/db.json", function (err) {
+  if (err) console.log(err);
+  else console.log("copy file succeed to" + os.tmpdir());
+});
+
 // See https://github.com/typicode/json-server#module
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-// const router = jsonServer.router(path.resolve(os.tmpdir() + "/db.json"));
+// const router = jsonServer.router("db.json");
+const router = jsonServer.router(path.resolve(os.tmpdir() + "/db.json"));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
